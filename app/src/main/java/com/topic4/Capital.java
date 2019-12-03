a24baf4dbac7b14b473e25aae816d18590a08d18
Shovan404 <shovanrj4@gmail.com>
2019-12-02 20:37:23 -0500

    Initial commit

---
+++ app/src/main/java/com/topic4/Capital.java
@@ -0,0 +1,14 @@
+package com.topic4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Capital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capital);
+    }
+}
---
+++ app/src/main/java/com/topic4/MainActivity.java
@@ -0,0 +1,73 @@
+package com.topic4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Button;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOpen;
+    public static String countries[]={
+            "Nepal","Kathmandu",
+            "India","Delhi",
+            "China","Beijing",
+            "UK","London",
+            "Canada","Ottawa",
+            "US","Washinton, D.C"
+    };
+    private Map<String,String> dictionary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lstcountries= findViewById(R.id.countries);
+
+        dictionary=new HashMap<>();
+        for(int i=0;i<countries.length;i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+        ArrayAdapter countryAdapter=new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                new ArrayList<String>(dictionary.keySet()));
+                lstcountries.setAdapter(countryAdapter);
+
+        lstcountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country=parent.getItemAtPosition(position).toString();
+                String capital = dictionary.get(country);
+                Toast.makeText(getApplicationContext(),capital.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new intent (MainActivity.this,capital.class);
+                intent
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
